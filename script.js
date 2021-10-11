227ee0e7a18a619b8fe35211094e98b687463943
Alemrv <alemrv@gmail.com>
2021-10-10 22:35:59 -0400

    Added html classes and id for numbers, operators, display and clear buttons.
    Modified display class, now is an id.
    
    Added Javascript logic display numbers when clicked.
    
    Added javascript logic to clear calculator numbers
    
    Added javescript logic to store values and perform one operation.

--- script.js
+++ script.js
@@ -1,20 +1,73 @@
+const numbers = document.querySelectorAll(".numbers");
+const display = document.getElementById("display");
+const clear = document.getElementById("clear");
+const operators = document.querySelectorAll(".operators");
+let operatorValue;
+const equal = document.getElementById("equal");
+let num1;
+let num2;
+
+equal.addEventListener("click", Operate);
+
+clear.addEventListener("click", Clear);
+
+numbers.forEach(number =>{
+    number.addEventListener("click", DisplayText);
+});
+
+operators.forEach(operator =>{
+    operator.addEventListener("click",() =>{
+        operatorValue = operator.textContent;
+        if (display.textContent !== "") {
+            if (typeof(num1) === "undefined") {
+                num1 = display.textContent;
+            }
+            else if (typeof(num2) === "undefined") {
+                num2 = display.textContent;
+            }
+            console.log(num1, num2);
+        }
+    });
+});
+
+function DisplayText(){
+    if (typeof(operatorValue) === "string") {
+        display.textContent = "";
+    }
+    if (display.textContent.length < 10) {
+        
+       display.textContent+= this.textContent;
+    }
+}
+
+function Clear(){
+    display.textContent = "";
+}
+
 function Add(a, b){
-    return a + b;
+    display.textContent = parseInt(a) + parseInt(b);
 }
 
 function Subtract(a, b){
-    return a - b;
+    display.textContent = parseInt(a) - parseInt(b);
 }
 
 function Multiply(a, b){
-    return a * b;
+    display.textContent = parseInt(a) * parseInt(b);
 }
 
 function Divide(a, b){
-    return a / b;
+    display.textContent = parseInt(a) / parseInt(b);
 }
 
-function Operate(num1, operator, num2){
+
+function Operate(first, operator, second){
+    operator = operatorValue;
+    first = num1;
+    second = num2;
+    if (typeof(num2) === "undefined") {
+        num2 = display.textContent;
+    }
     switch (operator) {
         case "+":
            return Add(num1, num2);
@@ -34,4 +87,4 @@ function Operate(num1, operator, num2){
     }
 }
 
-console.log(Operate(2, "/", 5));
+//console.log(Operate(2, "/", 5));
