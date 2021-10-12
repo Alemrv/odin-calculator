bc8dda073b26a3c6dd7a361418e01fc8f1638496
Alemrv <alemrv@gmail.com>
2021-10-11 23:29:42 -0400

    Modified 1 class in the html
    Fixed bugs on javascript

--- script.js
+++ script.js
@@ -2,39 +2,44 @@ const numbers = document.querySelectorAll(".numbers");
 const display = document.getElementById("display");
 const clear = document.getElementById("clear");
 const operators = document.querySelectorAll(".operators");
-let operatorValue;
 const equal = document.getElementById("equal");
-let num1;
-let num2;
+let displayNumbers = [];
+let operatorValue;
+let displayCheck = true;
+let operatorCheck = false;
 
 equal.addEventListener("click", Operate);
 
 clear.addEventListener("click", Clear);
 
+operators.forEach(operator =>{
+    operator.addEventListener("click", Operators);
+});
+
+function Operators(){
+    operatorValue = this.textContent;
+    if (display.textContent !== ""){
+        if (displayNumbers.length < 2) {
+            displayNumbers.push(display.textContent);
+        }
+        if (displayNumbers.length == 2) {
+            Operate();
+        }
+        displayCheck = false;
+    }
+    console.log(displayNumbers)
+}
+
 numbers.forEach(number =>{
     number.addEventListener("click", DisplayText);
 });
 
-operators.forEach(operator =>{
-    operator.addEventListener("click",() =>{
-        operatorValue = operator.textContent;
-        if (display.textContent !== "") {
-            if (typeof(num1) === "undefined") {
-                num1 = display.textContent;
-            }
-            else if (typeof(num2) === "undefined") {
-                num2 = display.textContent;
-            }
-            console.log(num1, num2);
-        }
-    });
-});
-
 function DisplayText(){
-    if (typeof(operatorValue) === "string") {
+    if (displayCheck === false) {
         display.textContent = "";
+        displayCheck = true;
     }
-    if (display.textContent.length < 10) {
+    if(display.textContent.length < 10 && displayCheck === true) {
         
        display.textContent+= this.textContent;
     }
@@ -42,44 +47,65 @@ function DisplayText(){
 
 function Clear(){
     display.textContent = "";
+    for (let i = 0; i < displayNumbers.length; i++) {
+        displayNumbers.splice(0, 2);
+    }
+    console.log(displayNumbers);
+    operatorValue = undefined;
+}
+
+function ContinueEqual(){
+    equal.addEventListener("click", ()=>{
+        displayNumbers[0] = display.textContent;
+        displayCheck = false;
+    });
+ 
+}
+
+function ContinueOperator(){
+ 
 }
 
 function Add(a, b){
     display.textContent = parseInt(a) + parseInt(b);
+    ContinueEqual();
 }
 
 function Subtract(a, b){
     display.textContent = parseInt(a) - parseInt(b);
+    ContinueEqual();
 }
 
 function Multiply(a, b){
     display.textContent = parseInt(a) * parseInt(b);
+    ContinueEqual();
 }
 
 function Divide(a, b){
     display.textContent = parseInt(a) / parseInt(b);
+    ContinueEqual();
 }
 
-
-function Operate(first, operator, second){
-    operator = operatorValue;
-    first = num1;
-    second = num2;
-    if (typeof(num2) === "undefined") {
-        num2 = display.textContent;
+function Operate(first, operator, second){   
+    if (displayNumbers.length < 2) {
+        displayNumbers.push(display.textContent);
     }
+    operator = operatorValue;
+    first = displayNumbers[0];
+    second = displayNumbers[1];
+    console.log(first,second);
     switch (operator) {
         case "+":
-           return Add(num1, num2);
+           return Add(first, second);
 
         case "-":
-           return Subtract(num1, num2);
+           return Subtract(first, second);
 
         case "*":
-           return Multiply(num1, num2);
+           return Multiply(first, second);
 
         case "/":
-           return Divide(num1, num2);
+           return Divide(first, second);
 
         default:
            return "error";
